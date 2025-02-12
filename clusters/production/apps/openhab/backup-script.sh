4e27c11eb8e9f2c1864865ce463e36045c6985cd
Jan Gustafsson <jannegpriv@gmail.com>
2025-02-12 22:48:04 +0100

    fix: update backup to use debian image with required tools

--- clusters/production/apps/openhab/backup-script.sh
+++ clusters/production/apps/openhab/backup-script.sh
@@ -14,26 +14,34 @@ OPENHAB_POD="openhab-production-0"
 NAS_PASS="${NAS_PASSWORD}"
 
 # Create backup using kubectl exec
+echo "Creating backup in OpenHAB pod..."
 kubectl exec -n openhab ${OPENHAB_POD} -- bash -c "
   mkdir -p /openhab/userdata/backup && \
-  /openhab/runtime/bin/client 'openhab:backup ${BACKUP_NAME}' && \
   cd /openhab/userdata/backup && \
+  /openhab/runtime/bin/client 'openhab:backup ${BACKUP_NAME}' && \
   tar czf ${BACKUP_NAME}.tar.gz ${BACKUP_NAME} && \
   rm -rf ${BACKUP_NAME}"
 
+# Create temporary directory
+TMP_DIR=$(mktemp -d)
+echo "Created temporary directory: ${TMP_DIR}"
+
 # Copy the backup from the pod
-kubectl cp openhab/${OPENHAB_POD}:/openhab/userdata/backup/${BACKUP_NAME}.tar.gz /tmp/${BACKUP_NAME}.tar.gz
+echo "Copying backup from pod..."
+kubectl cp openhab/${OPENHAB_POD}:/openhab/userdata/backup/${BACKUP_NAME}.tar.gz ${TMP_DIR}/${BACKUP_NAME}.tar.gz
 
 # Copy to NAS using sshpass
+echo "Copying backup to NAS..."
 export SSHPASS="${NAS_PASS}"
-sshpass -e scp -o StrictHostKeyChecking=no -P 4711 /tmp/${BACKUP_NAME}.tar.gz "${NAS_USER}@${NAS_HOST}:${NAS_PATH}/"
+sshpass -e scp -o StrictHostKeyChecking=no -P 4711 ${TMP_DIR}/${BACKUP_NAME}.tar.gz "${NAS_USER}@${NAS_HOST}:${NAS_PATH}/"
 
 # Clean up backups
 if [ $? -eq 0 ]; then
+    echo "Cleaning up temporary files..."
     kubectl exec -n openhab ${OPENHAB_POD} -- rm -f /openhab/userdata/backup/${BACKUP_NAME}.tar.gz
-    rm -f /tmp/${BACKUP_NAME}.tar.gz
+    rm -rf ${TMP_DIR}
     echo "Backup successfully copied to NAS: ${NAS_PATH}/${BACKUP_NAME}.tar.gz"
 else
-    echo "Failed to copy backup to NAS. Backup remains in OpenHAB pod and /tmp"
+    echo "Failed to copy backup to NAS. Backup remains in OpenHAB pod and ${TMP_DIR}"
     exit 1
 fi
